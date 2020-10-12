02538e1be4008102a3d3aa4b9b4a8a57af745040
Yuu-taremayu <taremayu.yuu@gmail.com>
2020-10-12 12:39:41 +0900

    remove anything

--- program/LagrangeInterpolationOnGF.py
+++ program/LagrangeInterpolationOnGF.py
@@ -18,31 +18,12 @@ def lagrange_interpolation(data_num, x, dataX, dataY):
     return L
 
 def main():
-    dataX = [0.0, 1.0]
-    dataY = [0.0, 3.0]
+    dataX = [GF(0.0), GF(1.0)]
+    dataY = [GF(0.0), GF(3.0)]
     data_num = len(dataX)
-    x = 122.0
+    x = GF(122.0)
     y = lagrange_interpolation(data_num, x, dataX, dataY)
     print(y)
-    '''
-    GF.space = 11
-    dataX = range(2)
-    rand = [random.randint(0, 10) for i in range(2)]
-    dataY = []
-    for i in range(len(rand)):
-        dataY.append(GF(rand[i]))
-    data_num = len(dataX)
-    for i in range(2):
-        print(f'x[{i}] = {dataX[i]}, y[{i}] = {dataY[i].value}')
-    x = GF(12)
-    y = lagrange_interpolation(data_num, x.value, dataX, dataY)
-    print(y)
-
-    for i in range(len(dataX)):
-        plt.scatter(dataX[i], dataY[i].value, label='dataset')
-    plt.scatter(x.value, y, label='interpolation')
-    plt.show()
-    '''
 
 if __name__ == '__main__':
     main()
