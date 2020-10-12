311c37a66c5559aed8075c2708016f120dbe8a9a
Yuu-taremayu <taremayu.yuu@gmail.com>
2020-10-12 17:18:44 +0900

    generate server id and polynomial

--- program/ShamirSecretSharing/ShamirSecretSharing.py
+++ program/ShamirSecretSharing/ShamirSecretSharing.py
@@ -1,7 +1,21 @@
+from GaloisField import GF
+import random
+
 def main():
+    s = 1
     k = 2
     n = 3
     p = 5
+    GF.space = p
+    random.seed(0)
+    server_id = [random.randint(0, p - 1) for i in range(n)]
+    while len(server_id) != len(set(server_id)):
+        server_id = [random.randint(0, p - 1) for i in range(n)]
+    print(f'server_id = {server_id}')
+    f_x = [s]
+    for i in range(k - 1):
+        f_x.append(random.randint(0, p - 1))
+    print(f'f_x = {f_x}')
 
 if __name__ == '__main__':
     main()
