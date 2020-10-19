608e1f9ce2c0def1e5b66448bd981348ef54ef7a
Yuu-taremayu <taremayu.yuu@gmail.com>
2020-10-19 11:28:02 +0900

    add lagrange

--- program/ShamirSecretSharing/ShamirSecretSharing.py
+++ program/ShamirSecretSharing/ShamirSecretSharing.py
@@ -36,6 +36,26 @@ def create_share(server_id, f_x):
 
     return w
 
+#
+# calculation of Lagrange Interpolation
+#
+def base_polynomial(data_num, i, x, dataX):
+    l = 1
+    for k in range(data_num):
+        if i != k:
+            l *= x - dataX[k]
+    return l
+
+def lagrange_interpolation(dataX, dataY):
+    data_num = len(dataX)
+    x = 0
+    l = 0
+    L = 0
+    for i in range(data_num):
+        l = base_polynomial(data_num, i, x, dataX) / base_polynomial(data_num, i, dataX[i], dataX)
+        L += dataY[i] * l
+    return L
+
 def main():
     #
     # define some constant
