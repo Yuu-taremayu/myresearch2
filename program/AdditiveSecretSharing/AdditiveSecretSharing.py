ae06f88e21733b74fe585a89cc38a644d28d4bab
Yuu-taremayu <taremayu.yuu@gmail.com>
2020-11-29 01:03:41 +0900

    implement Additive Secret Sharing

---
+++ program/AdditiveSecretSharing/AdditiveSecretSharing.py
@@ -0,0 +1,44 @@
+import random
+
+#
+# create share
+#
+def create_share(secret, n):
+    share = []
+    for i in range(1, n):
+        r = random.randint(-(2**63-1), 2**63-1)
+        share.append(r)
+    s = secret - sum(share)
+    share.insert(0, s)
+    return share
+
+def combine_share(share):
+    s = sum(share)
+    return s
+
+def main():
+    #
+    # define some constant
+    #
+    secret = 13
+    n = 10
+    random.seed(0)
+
+    #
+    # disperse imformation
+    #
+    share = create_share(secret, n)
+    print(f'share = {share}')
+
+    #
+    # reconstruct imformation
+    #
+    s = combine_share(share)
+
+    if secret == s:
+        print('success!')
+    else:
+        print('failed...')
+
+if __name__ == '__main__':
+    main()
