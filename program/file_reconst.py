02415de7ba8774d49c9f4f384f4eb02a2d2eee46
Yuu-taremayu <taremayu.yuu@gmail.com>
2021-01-07 23:27:58 +0900

    move

--- program/file_reconst.py
+++
@@ -1,8 +0,0 @@
-def int_to_hex(DEC):
-    re_HEX = ''
-    for i in range(len(DEC)):
-        h = str(format(DEC[i], 'x'))
-        re_HEX += h
-
-    with open('secret_reconst.hex', mode='w') as f:
-        f.write(re_HEX)
